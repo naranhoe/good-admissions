71fff156ff85af8c73f1529796658d6edd1993c7
Jorge Naranjo <j.naranjojr@gmail.com>
2015-12-08 13:14:54 -0500

    Add pagination to payments index page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,8 +10,10 @@ class PaymentsController < ApplicationController
   end
 
   def index
-    @payments = Student.all.map{ |s| s.payments }.flatten.sort_by(&:pay_date).reverse!
+    # @payments = Student.paginate(:page => params[:page], :per_page => 5)
+    @payments = Student.all.map{ |s| s.payments }.flatten.sort_by(&:pay_date).reverse!.paginate(:page => params[:page], :per_page => 5)
   end
+
   private
   def check_admin
     if current_user.try(:admin?) == false
