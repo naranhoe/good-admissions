d8ae3ad72d3c27e528828e6eb8a82b4f72699e26
Jorge Naranjo <j.naranjojr@gmail.com>
2015-12-10 09:21:03 -0500

    Search field no longer crashing

--- app/models/wire.rb
+++ app/models/wire.rb
@@ -2,6 +2,10 @@ class Wire < ActiveRecord::Base
   belongs_to :student
   before_create :set_sender
 
+  def self.search(search)
+    where("pay_date ILIKE ? OR created_at ILIKE ?", "%#{search}%", "%#{search}%")
+  end
+
   def set_sender
     if sender.blank?
       self.sender = self.student.full_name
