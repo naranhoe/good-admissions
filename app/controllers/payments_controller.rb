60ed65b0bdceedea596f8d9d906c50b983500f1b
Diego Lugo <diego.lugo.06@gmail.com>
2015-12-18 14:24:02 -0500

    Cohort validations and all payments bug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -11,15 +11,7 @@ class PaymentsController < ApplicationController
 
   def index
     @payments = Student.all.map{ |s| s.payments }.flatten.sort_by(&:pay_date).reverse!.paginate(:page => params[:page], :per_page => 10)
-    @search_for_payments = Student.all.find_by(params[:student_id]).payments[0]#.created_at
-    # raise
     @date = params[:month] ? Date.parse(params[:month]) : Date.today
-
-    # if params[:search] && params[:search].empty? == false
-    #   @search_payments = Student.search(params[:search]).order("created_at DESC")
-    # else
-    #   @search_payments = []
-    # end
   end
 
   private
--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,4 +1,5 @@
 class Cohort < ActiveRecord::Base
+  validates :name, :location, :cohort_number, :capacity, :start_date, presence: true
   has_many :students
   before_destroy :clear_cohort_on_students
 
